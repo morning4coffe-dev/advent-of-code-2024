29db8e98d0e80a7afdfb07779be1a820a77fd439
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-12-01 11:47:03 +0100

    Day01

---
+++ Day01_1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode24;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        foreach (var line in Extractor.GetInput())
+        {
+            var splitLine = line.Split("   ");
+            leftList.Add(int.Parse(splitLine[0]));
+            rightList.Add(int.Parse(splitLine[1]));
+        }
+
+        var sortedLeft = Sort(leftList);
+        var sortedRight = Sort(rightList);
+
+        var distance = 0;
+
+        for (var i = 0; i < sortedLeft.Count; i++)
+        {
+            distance += Math.Abs(sortedLeft[i] - sortedRight[i]);
+        }
+
+        Console.WriteLine(distance);
+    }
+
+    static List<int> Sort(List<int> list)
+    {
+        for (var i = 0; i < list.Count - 1; i++)
+        {
+            for (var j = 0; j < list.Count - i - 1; j++)
+            {
+                if (list[j] > list[j + 1])
+                {
+                    var temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
+
+        return list;
+    }
+}
---
+++ Day01_2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode24;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        foreach (var line in Extractor.GetInput())
+        {
+            var splitLine = line.Split("   ");
+            leftList.Add(int.Parse(splitLine[0]));
+            rightList.Add(int.Parse(splitLine[1]));
+        }
+
+        var score = 0;
+
+        foreach (var left in leftList)
+        {
+            var localScore = 0;
+            foreach (var right in rightList)
+            {
+                if (left == right)
+                {
+                    localScore++;
+                }
+            }
+            score += left * localScore;
+        }
+
+        Console.WriteLine(score);
+    }
+}
