64f38830a7d5c7493c74f5d9178d3592a566810b
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-12-02 13:11:58 +0100

    Day02

---
+++ Day02_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode24;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var score = 0;
+
+        foreach (var line in Extractor.GetInput())
+        {
+            var splitLine = line.Split(" ");
+
+            bool? isIncreasing = null;
+            var isSafe = true;
+
+            for (var i = 0; i < splitLine.Length; i++)
+            {
+                if (i + 1 >= splitLine.Length)
+                {
+                    break;
+                }
+
+                var currentLevel = int.Parse(splitLine[i]);
+                var nextLevel = int.Parse(splitLine[i + 1]);
+
+                var difference = nextLevel - currentLevel;
+
+                if (isIncreasing == null)
+                {
+                    isIncreasing = difference > 0;
+                }
+                else
+                if (isIncreasing != (difference > 0))
+                {
+                    isSafe = false;
+                    break;
+                }
+
+                if (Math.Abs(difference) < 1 || Math.Abs(difference) > 3)
+                {
+                    isSafe = false;
+                    break;
+                }
+            }
+
+            if (isSafe)
+            {
+                score++;
+            }
+        }
+
+        Console.WriteLine(score);
+    }
+}
---
+++ Day02_2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode24;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var score = 0;
+
+        foreach (var line in Extractor.GetInput())
+        {
+            var levels = line.Split(" ").Select(int.Parse).ToArray();
+
+            if (IsSafe(levels))
+            {
+                score++;
+                continue;
+            }
+
+            for (int i = 0; i < levels.Length; i++)
+            {
+                var modifiedLevels = levels.Where((_, index) => index != i).ToArray();
+                if (IsSafe(modifiedLevels))
+                {
+                    score++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(score);
+    }
+
+    static bool IsSafe(int[] levels)
+    {
+        bool? isIncreasing = null;
+
+        for (int i = 0; i < levels.Length - 1; i++)
+        {
+            int currentLevel = levels[i];
+            int nextLevel = levels[i + 1];
+            int difference = nextLevel - currentLevel;
+
+            if (isIncreasing == null)
+            {
+                isIncreasing = difference > 0;
+            }
+            else if (isIncreasing != (difference > 0))
+            {
+                return false;
+            }
+
+            if (Math.Abs(difference) < 1 || Math.Abs(difference) > 3)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
