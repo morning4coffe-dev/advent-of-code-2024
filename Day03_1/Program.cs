b14a2c8555cc7b1454c57e950a17e26ba9d0e55b
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-12-03 10:40:23 +0100

    Day03

---
+++ Day03_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode24;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        var score = 0;
+
+        var mulInstructions = MulRegex().Matches(Extractor.GetInput());
+
+        foreach (Match mulInstruction in mulInstructions)
+        {
+            var values = mulInstruction.Value.Split(',');
+            var a = int.Parse(values[0].Substring(4));
+            var b = int.Parse(values[1].Substring(0, values[1].Length - 1));
+            score += a * b;
+        }
+
+        Console.WriteLine(score);
+    }
+
+    [GeneratedRegex("mul\\(\\d+,\\d+\\)")]
+    internal static partial Regex MulRegex();
+}
---
+++ Day03_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode24;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        var score = 0;
+
+        var instructions = FunctionsRegex().Matches(Extractor.GetInput());
+        var isDo = true;
+
+        foreach (Match instruction in instructions)
+        {
+            if (instruction.Value == "do()" || instruction.Value == "don't()")
+            {
+                isDo = instruction.Value == "do()";
+                continue;
+            }
+
+            if (isDo)
+            {
+                var values = instruction.Value.Split(',');
+                var a = int.Parse(values[0].Substring(4));
+                var b = int.Parse(values[1].Substring(0, values[1].Length - 1));
+                score += a * b;
+            }
+        }
+
+        Console.WriteLine(score);
+    }
+
+    [GeneratedRegex("(do\\(\\)|don't\\(\\)|mul\\(\\d+,\\s*\\d+\\))")]
+    internal static partial Regex FunctionsRegex();
+}
