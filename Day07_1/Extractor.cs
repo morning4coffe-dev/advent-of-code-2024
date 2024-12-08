240f8a0d422f9e2985aa1528a569d3454d732725
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-12-08 11:09:53 +0100

    Day07

---
+++ Day07_1/Extractor.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode24;
+
+internal class Extractor
+{
+    public static string[] GetInput()
+    {
+        var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var projectDirectory = Directory.GetParent(currentDirectory)?.Parent?.Parent?.Parent?.FullName;
+
+        var filePath = Path.Combine(projectDirectory, "input.txt");
+
+        return File.ReadAllLines(filePath);
+    }
+}
