04909f13383e1906ea86065de306c2965aedebe6
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-12-09 21:54:34 +0100

    Day08

---
+++ Day08_1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace AdventOfCode24;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Extractor.GetInput();
+        int width = input[0].Length;
+        int height = input.Length;
+
+        var grid = new char[width, height];
+        var locations = new Dictionary<char, List<(int x, int y)>>();
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                var currentInput = input[y][x];
+                if (currentInput != '.')
+                {
+                    if (locations.ContainsKey(currentInput))
+                    {
+                        locations[currentInput].Add((x, y));
+                    }
+                    else
+                    {
+                        locations.Add(currentInput, [(x, y)]);
+                    }
+                }
+
+                grid[x, y] = currentInput;
+            }
+        }
+
+        var antinodes = new HashSet<(int x, int y)>();
+
+        foreach (var type in locations)
+        {
+            var location = type.Value;
+            for (int i = 0; i < location.Count; i++)
+            {
+                for (int j = i + 1; j < location.Count; j++)
+                {
+                    var distance = (location[j].x - location[i].x, location[j].y - location[i].y);
+
+                    antinodes.Add((location[i].x - distance.Item1, location[i].y - distance.Item2));
+                    antinodes.Add((location[j].x + distance.Item1, location[j].y + distance.Item2));
+                }
+            }
+        }
+
+        Console.WriteLine(antinodes.Where(IsWithinBounds).Count());
+
+        bool IsWithinBounds((int x, int y) location)
+        {
+            return location.x >= 0 && location.x < width && location.y >= 0 && location.y < height;
+        }
+    }
+}
